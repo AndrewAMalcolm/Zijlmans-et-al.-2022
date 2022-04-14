b73c264aabee5dbf356a088f53ee9117d1dccd10
AndrewAMalcolm <102665131+AndrewAMalcolm@users.noreply.github.com>
2022-04-14 15:01:45 +0100

    Update forGenomicAnnotation.r

--- forGenomicAnnotation.r
+++ forGenomicAnnotation.r
@@ -1,28 +1,29 @@
+#RScript for annotating peaks with genomic context using the ChIPSeeker package (Yu et al., 2015)
+
 BiocManager::install('TxDb.Hsapiens.UCSC.hg38.knownGene')
 BiocManager::install(ChIPseeker)
 library(ChIPseeker)
 
 library(TxDb.Hsapiens.UCSC.hg38.knownGene)
 
+
+#load library
 txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
 
-
+#read in filenames for peak bed files
 files <- list.files(pattern = '120122')
 files <- files[-2]
 test <- read.table('080122_random.bed')
 names(files) <- c( "Naive", 'Primed', "Random")
 
-library(org.Hs.eg.db)
-library(EnsDb.Hsapiens.v75)
-edb <- EnsDb.Hsapiens.v75
-seqlevelsStyle(edb) <- "UCSC"
 
+#annotate each peak for each file with genomic context 
 
 peakAnnoList <- lapply(files, annotatePeak, TxDb=txdb,  tssRegion = c(-3000, 3000), level = 'gene')
 library(tidyverse)
 peakAnnoList
 
-
+#plot stacked bar chart 
 plotAnnoBar(peakAnnoList)
 
 
